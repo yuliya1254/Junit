078f6ab20cb730f3206932b1d41b688266b5c021
Julia Menschikova <yu.menschikowa2015@yandex.ru>
2023-11-18 19:38:58 +0300

    add alure

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,22 +2,54 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = "https://www.wildberries.ru";
+    @BeforeEach
+    public void beforeAllCase() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "100");
+        Configuration.holdBrowserOpen = false;
+        Configuration.browserSize = System.getProperty("windowSize", "1920x1080");
+        Configuration.timeout = 10000;
+        Configuration.remote = System.getProperty("webDriver", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void enableAllure() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterEach
-    void afterEach() {
-        Selenide.closeWebDriver();
-        Selenide.clearBrowserCookies();
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        if (!Configuration.browser.equalsIgnoreCase("firefox")){
+            Attach.browserConsoleLogs();
+        }
+        Attach.addVideo();
+        closeWebDriver();
     }
 
 }
